7c23879b69e373888b64d5acffcd256ed1666fa7
Christopher J <chrisbjones1987@yahoo.com>
2026-01-09 14:55:48 -0600

    Added styling to the client

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="app_wrapper">
         <Navigation />
         <main>
           <Routes>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,12 +2,17 @@ import { Link } from 'react-router-dom';
 
 function Navigation() {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/services">Services</Link>
-      <Link to="/resources">Resources</Link>
-      <Link to="/contact">Contact</Link>
+    <nav className="navigation">
+      <div className="nav-brand">
+        <Link to="/">AG Reese & Associates, P.C.</Link>
+      </div>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/services">Services</Link>
+        <Link to="/resources">Resources</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
     </nav>
   );
 }
