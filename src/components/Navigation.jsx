939fdf7ff84f89517211bcb4b35c2548feb9da97
Christopher J <chrisbjones1987@yahoo.com>
2026-01-10 21:03:46 -0600

    Updated navigation styling to client

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,6 +5,7 @@ function Navigation() {
     <nav className="navigation">
       <div className="nav-brand">
         <Link to="/">AG Reese & Associates, P.C.</Link>
+        <h5 className='slogan'>Integrity at its best</h5>
       </div>
       <div className="nav-links">
         <Link to="/">Home</Link>
