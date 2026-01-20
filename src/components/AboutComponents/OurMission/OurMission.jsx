b63faaa1ca2890cfaddab26fc3c5fbe4bfe9a7e3
Christopher J <chrisbjones1987@yahoo.com>
2026-01-20 14:36:25 -0600

    Added our mission component

---
+++ src/components/AboutComponents/OurMission/OurMission.jsx
@@ -0,0 +1,16 @@
+import "./OurMission.css";
+import Divider from "../AboutDivider/AboutDivider";
+
+function OurMission () {
+    return (
+        <div>
+            <div className="our-mission">
+                <h4 className="our-mission-header">Our Mission</h4>
+                <h5 className="our-mission-content">To provide exceptional accounting , tax, and audit serviceswith unwavering accuracy and integrity, while building lasting relationships with our clients and contributing to the success of our Richmond community.</h5>
+            </div>
+            <Divider />
+        </div>
+    )
+}
+
+export default OurMission;
--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/AboutComponents/AboutHeader/Header";
 import AboutOurFirm from "../components/AboutComponents/AboutOurFirm/AboutOurFirm";
+import OurMission from "../components/AboutComponents/OurMission/OurMission";
 
 function About() {
   return (
     <div>
       <Header />
       <AboutOurFirm />
+      <OurMission />
     </div>
   );
 }
