3f6cb2b7cae9c7e442faaddf15990d834ea16f1e
Christopher J <chrisbjones1987@yahoo.com>
2026-01-26 19:41:54 -0600

    Added financial services component

--- src/views/Services.jsx
+++ src/views/Services.jsx
@@ -1,11 +1,13 @@
 import ServicesHeader from "../components/ServicesComponents/ServicesHeader/ServicesHeader";
 import TaxServices from "../components/ServicesComponents/TaxServicesComponent/TaxServices";
+import FinancialServices from "../components/ServicesComponents/FinancialServicesComponent/FinancialServices";
 
 function Services() {
   return (
     <div>
       <ServicesHeader />
       <TaxServices />
+      <FinancialServices />
     </div>
   );
 }
