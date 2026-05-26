33d3d85a616e55806135c0d1bf367c83ccf8e9e3
Christopher J <chrisbjones1987@yahoo.com>
2026-05-26 14:49:17 -0500

    Updated Services components

--- src/components/ServicesComponents/AdvisoryComponent/AdvisoryServices.jsx
+++ src/components/ServicesComponents/AdvisoryComponent/AdvisoryServices.jsx
@@ -3,25 +3,20 @@ import Divider from "../../ReusableComponents/DividerComponent/Divider";
 
 function AdvisoryServices () {
     return (
-        <div>
-            <h3 className="advisory-service-header">Advisory Services</h3>
-            <h5 className="advisory-service-content">Strategic guidance for business planning and financial decisions.</h5>
-            <Divider />
-            <div className="advisory-services">
-                <h4 className="advisory-header">Consultations</h4>
-                <h5 className="advisory-content">Trusted guidance on tax planning, business structure, and financial strategy. Whether you're starting a business or planning for the future, we provide the insights you need to succeed.</h5>
-                <div className="advisory-bullets">
-                    <ul className="advisory-bullets-left">
-                        <li>Merger and acquisition support</li>
-                        <li>Financial planning</li>
-                        <li>Succession planning</li>
-                    </ul>
-                    <ul className="advisory-bullets-right">
-                        <li>Business formation advice</li>
-                        <li>Process improvement</li>
-                        <li>Technology consulting</li>
-                    </ul>
-                </div>
+        <div className="advisory-services">
+            <h4 className="advisory-header">Advisory Services</h4>
+            <h5 className="advisory-content">Trusted guidance on tax planning, business structure, and financial strategy. Whether you're starting a business or planning for the future, we provide the insights you need to succeed.</h5>
+            <div className="advisory-bullets">
+                <ul className="advisory-bullets-left">
+                    <li>Fractional accounting</li>
+                    <li>Financial planning</li>
+                    <li>Succession planning</li>
+                </ul>
+                <ul className="advisory-bullets-right">
+                    <li>Business formation advice</li>
+                    <li>Process improvement</li>
+                    <li>Technology consulting</li>
+                </ul>
             </div>
         </div>
     )
