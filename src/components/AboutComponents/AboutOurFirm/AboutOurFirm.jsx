97712dac78d974eb235c85608a06e163607a88a8
Christopher J <chrisbjones1987@yahoo.com>
2026-05-26 14:16:37 -0500

    Updated About component

--- src/components/AboutComponents/AboutOurFirm/AboutOurFirm.jsx
+++ src/components/AboutComponents/AboutOurFirm/AboutOurFirm.jsx
@@ -4,9 +4,10 @@ import Divider from "../../ReusableComponents/DividerComponent/Divider";
 function AboutOurFirm () {
     return (
         <div>
-            <h3 className="our-firm-header">About Our Firm</h3>
-            <h5 className="our-firm-content">Founded in Richmond, Virginia, A.G. Reese & Associates, P.C. has been serving the local community with integrity and expertise. As a small, dedicated firm, we pride ourselves on building long-term relationship swith our clients and delivering accurate, reliable accounting services.</h5>
-            <h5 className="our-firm-content">We are proud to be SWaM certified and active members of both the AICPA and the Virginia Society of CPAs, demonstrating our committment to professional excellence and community involvement.</h5>
+            <h3 className="our-firm-header">History</h3>
+            <h5 className="our-firm-content">
+                A. G. Reese & Associates P.C is a full service Certiﬁed Public Accounting ﬁrm that has been delivering exceptional, reliable, and timely professional services to individuals, businesses, and non-proﬁt organizations, all at a fair and transparent fee for over 40 years. Alan G. Reese, organized A. G. Reese & Associates, P.C., in 1983 after working for Price Waterhouse & Coopers (a big four accounting ﬁrm). His Vision and leadership have resulted in the development of a multi-talented professional staff with unparalleled dedication to the delivery of the highest quality of service available. 
+            </h5>
             <Divider />
         </div>
     )
--- src/components/AboutComponents/OurMission/OurMission.jsx
+++ src/components/AboutComponents/OurMission/OurMission.jsx
@@ -6,7 +6,12 @@ function OurMission () {
         <div>
             <div className="our-mission">
                 <h4 className="our-mission-header">Our Mission</h4>
-                <h5 className="our-mission-content">To provide exceptional accounting , tax, and audit serviceswith unwavering accuracy and integrity, while building lasting relationships with our clients and contributing to the success of our Richmond community.</h5>
+                <h5 className="our-mission-content">
+                    We are committed to building long-term relationships grounded in trust, integrity, and personalized attention, helping our clients achieve clarity, compliance, and conﬁdence in every ﬁnancial decision.
+                </h5>
+                <h5 className="our-mission-content">
+                    At our ﬁrm, you are more than a client, you are family. Your goals matter to us, and we are here to support your success every step of the way.
+                </h5>
             </div>
             <Divider />
         </div>
