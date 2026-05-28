a19bcf2ccefe4ae15f31f4bf643c296a592ffb27
Christopher J <chrisbjones1987@yahoo.com>
2026-05-28 13:14:18 -0500

    Updated About Components

--- src/components/AboutComponents/AboutHeader/Header.jsx
+++ src/components/AboutComponents/AboutHeader/Header.jsx
@@ -6,7 +6,11 @@ function Header () {
             <div className="about-header-content">
                 <h2 className="about-header-title">About Us</h2>
                 <h4 className="about-header-subtitle">
-                    A.G. Reese & Associates, P.C. is a licensed CPA firm based in Richmond, Virginia, providing personalized accounting, tax, and audit services to individuals and businesses. With decades of experience, we are SWaM certified and actively involved in leading professional organizations, including the AICPA, Virginia Society of CPAs (Richmond Chapter), and the National Association of Black Accountants. Our Peer Review enrollment reflects our commitment to quality, integrity, and professional excellence.
+                    {/* A. G. Reese & Associates, P.C. is a licensed CPA firm based in Richmond, Virginia, providing personalized accounting, tax, and audit services to individuals and businesses. With decades of experience, we are SWaM certified and actively involved in leading professional organizations, including the AICPA, Virginia Society of CPAs(Richmond Chapter), and the National Association of Black Accountants. Our Peer Review enrollment reflects our commitment to quality, integrity, and professional excellence. */}
+                    A. G. Reese & Associates, P.C. is a licensed CPA firm based in Richmond, Virginia, providing personalized accounting, tax, and audit services to individuals and businesses. Our Peer Review enrollment reflects our commitment to quality, integrity, and professional excellence.
+{/* <br />
+<br /> */}
+{/* A. G. Reese & Associates, P.C. is a trusted CPA firm serving Richmond, Virginia. With decades of experience, we provide personalized accounting, tax, and audit services to individuals and businesses. */}
                 </h4>
             </div>
         </div>
--- src/components/AboutComponents/AboutOurFirm/AboutOurFirm.jsx
+++ src/components/AboutComponents/AboutOurFirm/AboutOurFirm.jsx
@@ -1,3 +1,5 @@
+// This section was changed from About Our Firm to History
+
 import "./AboutOurFirm.css";
 import Divider from "../../ReusableComponents/DividerComponent/Divider";
 
@@ -6,7 +8,10 @@ function AboutOurFirm () {
         <div>
             <h3 className="our-firm-header">History</h3>
             <h5 className="our-firm-content">
-                A. G. Reese & Associates P.C is a full service Certiﬁed Public Accounting ﬁrm that has been delivering exceptional, reliable, and timely professional services to individuals, businesses, and non-proﬁt organizations, all at a fair and transparent fee for over 40 years. Alan G. Reese, organized A. G. Reese & Associates, P.C., in 1983 after working for Price Waterhouse & Coopers (a big four accounting ﬁrm). His Vision and leadership have resulted in the development of a multi-talented professional staff with unparalleled dedication to the delivery of the highest quality of service available. 
+                A. G. Reese & Associates P.C is a full service Certiﬁed Public Accounting ﬁrm that has been delivering exceptional, reliable, and timely professional services to individuals, businesses, and non-proﬁt organizations, all at a fair and transparent fee for over 40 years. Alan G. Reese, organized A. G. Reese & Associates, P.C., in 1983 after working for Price Waterhouse & Coopers(a big four accounting ﬁrm). His Vision and leadership have resulted in the development of a multi-talented professional staff with unparalleled dedication to the delivery of the highest quality of service available.
+            </h5>
+            <h5 className="our-firm-content">
+                With decades of experience, we are SWaM certified and actively involved in leading professional organizations, including the AICPA, Virginia Society of CPAs(Richmond Chapter), and the National Association of Black Accountants.
             </h5>
             <Divider />
         </div>
