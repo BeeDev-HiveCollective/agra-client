b21f43c14b8ed5eb142ecca3d2925ad5aa5fd7e9
Christopher J <chrisbjones1987@yahoo.com>
2026-06-02 17:44:23 -0500

    Added Meet Our Team component options

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,7 +1,9 @@
 import Header from "../components/AboutComponents/AboutHeader/Header";
 import History from "../components/AboutComponents/History/History";
 import OurMission from "../components/AboutComponents/OurMission/OurMission";
-import OurTeam from "../components/AboutComponents/OurTeam/OurTeam";
+// import OurTeam from "../components/AboutComponents/OurTeam/OurTeam";
+import OurTeam from "../components/AboutComponents/OurTeam/OurTeam_B";
+// import OurTeam from "../components/AboutComponents/OurTeam/OurTeam_C";
 import CoreValues from "../components/AboutComponents/CoreValues/CoreValues";
 
 function About() {
