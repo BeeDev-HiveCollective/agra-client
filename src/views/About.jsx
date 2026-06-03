75fcb0c6916b97891ac8a42f8b6d94c4fd4991fa
Christopher J <chrisbjones1987@yahoo.com>
2026-06-03 11:51:58 -0500

    Updated OurTeam component and styling

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,9 +1,7 @@
 import Header from "../components/AboutComponents/AboutHeader/Header";
 import History from "../components/AboutComponents/History/History";
 import OurMission from "../components/AboutComponents/OurMission/OurMission";
-// import OurTeam from "../components/AboutComponents/OurTeam/OurTeam";
-import OurTeam from "../components/AboutComponents/OurTeam/OurTeam_B";
-// import OurTeam from "../components/AboutComponents/OurTeam/OurTeam_C";
+import OurTeam from "../components/AboutComponents/OurTeam/OurTeam";
 import CoreValues from "../components/AboutComponents/CoreValues/CoreValues";
 
 function About() {
