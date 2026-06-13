956c9c87dc889f744a1e59695599c96b969e10a8
Christopher J <chrisbjones1987@yahoo.com>
2026-06-13 13:44:44 -0500

    Updated Header and History components in AboutComponents

--- src/components/AboutComponents/AboutHeader/Header.jsx
+++ src/components/AboutComponents/AboutHeader/Header.jsx
@@ -6,7 +6,7 @@ function Header () {
             <div className="about-header-content">
                 <h2 className="about-header-title">About Us</h2>
                 <h4 className="about-header-subtitle">
-                    A. G. Reese & Associates, P.C. is a licensed CPA firm based in Richmond, Virginia, providing personalized accounting, tax, and audit services to individuals and businesses. Our Peer Review enrollment reflects our commitment to quality, integrity, and professional excellence.
+                    A. G. Reese & Associates, P.C. is a licensed CPA firm based in Richmond, Virginia, providing personalized accounting, tax, and audit services to individuals and businesses. Our AICPA Peer Review enrollment reflects our commitment to quality, integrity, and professional excellence.
                 </h4>
             </div>
         </div>
--- src/components/AboutComponents/History/History.jsx
+++ src/components/AboutComponents/History/History.jsx
@@ -8,8 +8,9 @@ function History () {
             <h5 className="history-content">
                 A. G. Reese & Associates P.C is a full service Certiﬁed Public Accounting ﬁrm that has been delivering exceptional, reliable, and timely professional services to individuals, businesses, and non-proﬁt organizations, all at a fair and transparent fee for over 40 years. Alan G. Reese, organized A. G. Reese & Associates, P.C., in 1983 after working for Price Waterhouse & Coopers(a big four accounting ﬁrm). His Vision and leadership have resulted in the development of a multi-talented professional staff with unparalleled dedication to the delivery of the highest quality of service available.
             </h5>
+            <br />
             <h5 className="history-content">
-                With decades of experience, we are SWaM certified and actively involved in leading professional organizations, including the AICPA, Virginia Society of CPAs(Richmond Chapter), and the National Association of Black Accountants.
+                With decades of experience, we are SWaM certified and actively involved in leading professional organizations, including the AICPA, Virginia Society of CPAs (Richmond Chapter), and the National Association of Black Accountants.
             </h5>
             <Divider />
         </div>
